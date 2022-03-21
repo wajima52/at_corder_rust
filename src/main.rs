c1db66436e794ad3bbcdbfba2fa3e168fbce6132
wajima <mikkywaji@icloud.com>
2022-03-21 09:05:05 +0900

    マーク数をカウントする関数を作成

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,44 @@ use std::io;
 #[derive(PartialEq)]
 #[derive(Debug)]
 enum Mark {
-    circle, cross
+    Circle,
+    Cross,
 }
 
-
+#[derive(PartialEq)]
+#[derive(Debug)]
+struct MarkCount {
+    mark: Mark,
+    count: i32,
+}
 
 fn determine_mark(mark: &str) -> Result<Mark, &'static str> {
     if mark == String::from("○") {
-        Ok(Mark::circle)
-    } else if mark == "×"{
-        Ok(Mark::cross)
+        Ok(Mark::Circle)
+    } else if mark == "×" {
+        Ok(Mark::Cross)
     } else {
         Err("Invalid Input")
     }
 }
 
-#[test]
-fn determine_mark_test() {
-    assert_eq!(determine_mark("○"), Ok(Mark::circle));
-    assert_eq!(determine_mark("×"), Ok(Mark::cross));
-    assert_eq!(determine_mark("a"), Err("Invalid Input"));
-    assert_eq!(determine_mark(""), Err("Invalid Input"));
+fn calculate_mark_count(mark_count: MarkCount, mark: Mark) -> MarkCount {
+    if mark_count.mark == mark {
+        MarkCount { mark: mark_count.mark, count: mark_count.count + 1 }
+    } else {
+        MarkCount { mark: mark, count: 1 }
+    }
 }
+
+// fn count_continuous_mark(input: String) -> Option<MarkCount> {
+//     let mut mark_count: MarkCount;
+//     for char in input.as_str().chars() {
+//         let mark = determine_mark(&char.to_string());
+//         match mark {
+//             Mark::circle => MarkCount{}
+//         }
+//     }
+// }
 
 fn main() {
     let mut input = String::new();
@@ -32,6 +48,9 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read line");
 
+    for i in input.as_str().chars() {
+        println!("{}", i.to_string())
+    }
     let mark = determine_mark(input.trim());
     if mark.is_ok() {
         println!("Valid Mark")
@@ -40,3 +59,42 @@ fn main() {
     }
 }
 
+#[test]
+fn determine_mark_test() {
+    assert_eq!(determine_mark("○"), Ok(Mark::Circle));
+    assert_eq!(determine_mark("×"), Ok(Mark::Cross));
+    assert_eq!(determine_mark("a"), Err("Invalid Input"));
+    assert_eq!(determine_mark(""), Err("Invalid Input"));
+}
+
+#[test]
+fn calculate_mark_count_test() {
+    assert_eq!(
+        calculate_mark_count(
+            MarkCount { mark: Mark::Circle, count: 2 },
+            Mark::Circle
+        ),
+        MarkCount { mark: Mark::Circle, count: 3 }
+    );
+    assert_eq!(
+        calculate_mark_count(
+            MarkCount { mark: Mark::Cross, count: 2 },
+            Mark::Cross
+        ),
+        MarkCount { mark: Mark::Cross, count: 3 }
+    );
+    assert_eq!(
+        calculate_mark_count(
+            MarkCount { mark: Mark::Cross, count: 2 },
+            Mark::Circle
+        ),
+        MarkCount { mark: Mark::Circle, count: 1 }
+    );
+}
+
+// #[test]
+// fn count_continuous_mark_test() {
+//     assert_eq!(count_continuous_mark(String::from("○○○")), MarkCount { mark: Mark::circle, count: 3 });
+//     assert_eq!(count_continuous_mark(String::from("×××")), MarkCount { mark: Mark::cross, count: 3 });
+//     assert_eq!(count_continuous_mark(String::from("×○×")), MarkCount { mark: Mark::cross, count: 1 });
+// }
