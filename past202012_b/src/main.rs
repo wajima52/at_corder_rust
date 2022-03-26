dc809b63c9b95d001c0be16cc5b5dcbeac217e0a
wajima <mikkywaji@icloud.com>
2022-03-26 09:28:26 +0900

    同じ文字列を削除した後に後ろに文字を加えるメソッドを実装

--- past202012_b/src/main.rs
+++ past202012_b/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+fn add_character_after_remove_same<'a> (string: String, added_char: &str) -> String {
+    let string = string.replace(added_char, "");
+    [string, added_char.to_string()].concat()
+}
+
+#[test]
+fn add_character_after_remove_same_test() {
+    assert_eq!(add_character_after_remove_same(String::from("abc"), "a"), "bca");
+    assert_eq!(add_character_after_remove_same(String::from("abbbc"), "b"), "acb");
+}
