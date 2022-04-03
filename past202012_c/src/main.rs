3d03f146e64d08a257e761a2506a9af3ff294a8b
wajima <mikkywaji@icloud.com>
2022-04-03 16:18:50 +0900

    進法に用いる文字列を返すメソッドを作成

--- past202012_c/src/main.rs
+++ past202012_c/src/main.rs
@@ -1,3 +1,41 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+/*
+進法に用いる文字列をvec型で返す。
+base_number: X進法のXの数値
+ */
+fn get_strings_to_positional_notation(base_number: u8) -> Result<Vec<char>, &'static str> {
+    if base_number < 2 {
+        Err("Too Small Number")
+    } else if base_number <= 10 {
+        Ok((0..(base_number)).map(|num| (num + b'0') as char).collect::<Vec<_>>())
+    } else if base_number <= 36{
+        let mut numbers: Vec<char> = (0..10).map(|num| (num + b'0') as char).collect::<Vec<_>>();
+        let mut alphabets: Vec<char> = (0..(base_number - 10)).map(|num| (num + b'A') as char).collect::<Vec<_>>();
+        numbers.append(&mut alphabets);
+        Ok(numbers)
+    } else {
+        Err("Too Large Number")
+    }
+}
+
+#[test]
+fn get_strings_to_positional_notation_test() {
+    // 0,1進法はエラーを返す
+    assert_eq!(get_strings_to_positional_notation(0), Err("Too Small Number"));
+    assert_eq!(get_strings_to_positional_notation(1), Err("Too Small Number"));
+    // 2進法 ~ 36進法の文字列が正しいかをテスト
+    assert_eq!(get_strings_to_positional_notation(2), Ok(vec!['0', '1']));
+    assert_eq!(get_strings_to_positional_notation(10), Ok(vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']));
+    assert_eq!(get_strings_to_positional_notation(16), Ok(vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']));
+    assert_eq!(get_strings_to_positional_notation(36), Ok(vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']));
+    // 37進法はエラ＝を返す
+    assert_eq!(get_strings_to_positional_notation(37), Err("Too Large Number"));
+}
+
+
+
+
